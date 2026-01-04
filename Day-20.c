55746446449f5cc2fec9d5c6de99bff1dfe81fcc
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-04 15:15:01 +0600

    Update Day-20.c

--- Day-20.c
+++ Day-20.c
@@ -23,9 +23,34 @@ for(int index=1; index<5; index++){
 
 
 
+// Array input 
+
+int numbers[5];
+
+// using normal scanf for input
+
+scanf("%d", &numbers[0]);
+scanf("%d", &numbers[1]);
+scanf("%d", &numbers[2]);
+scanf("%d", &numbers[3]);
+scanf("%d", &numbers[4]);
+
+// using loop for input
+for(int index=0;index<5;index++){
+    printf("number[%d] = " , index);
+scanf("%d", &numbers[index]);
+    
+}
+
+
+for(int index=0;index<5;index++){
+printf("%d\n", numbers[index]);
+}
+    
 
 
     return 0;
 
 }
+
 
