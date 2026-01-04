ebb884acf3725659c3c71d66f3cd256653c220e6
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-04 14:39:25 +0600

    Update Day-20.c

--- Day-20.c
+++ Day-20.c
@@ -14,9 +14,18 @@ printf("%d\n", numbers[4]);
 
 
 
+        // using loop in array
+int numbers[5] ={10,20,30,40,50};
+
+for(int index=1; index<5; index++){
+    printf("%d\n", numbers[index]);
+}
+
+
 
 
 
     return 0;
 
 }
+
